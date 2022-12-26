6fa1890640b2400a07523b97d1ad33f8cf2d1741
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2022-12-25 23:52:32 -0500

    Weekly remaining time

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,8 +2,11 @@
     <header class="fixed top-0 z-40 w-full border-b dark:border-gray-800 bg-white dark:bg-gray-900">
         <div class="flex items-center justify-between h-16 px-4 lg:px-8">
             <h1 class="text-2xl font-black">NeoBigben</h1>
-            <div class="flex items-center gap-4">
-                <div class="font-bold text-xl">02:00</div>
+            <div class="flex items-center gap-6">
+                <div class="flex gap-2 items-center font-bold text-xl">
+                    <IClock />
+                    {{ remainingTime }}
+                </div>
                 <!-- 40h00 - 02h00 = 38h00 -->
                 <!-- Total de la semaine -->
                 <!-- Heures faites -->
@@ -14,3 +17,13 @@
         </div>
     </header>
 </template>
+
+<script lang="ts" setup>
+import IClock from '@/assets/svg/clock.svg?component';
+import { storeToRefs } from 'pinia';
+import { useStore } from '@/stores/index';
+
+const store = useStore();
+
+const { remainingTime } = storeToRefs(store);
+</script>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -4,9 +4,28 @@ import { v4 as uuidv4 } from 'uuid';
 export const useStore = defineStore('store', {
     state: () => {
         return {
+            weeklyHours: '40:00',
             projects: [] as Project[],
             entries: [] as Entry[],
+            // TODO: Sort by start_time
+            // TODO: Add space between not subsequent timeslot
         };
+    },
+    getters: {
+        remainingTime(): string {
+            const { $moment } = useNuxtApp();
+
+            const total = this.entries.reduce((acc, e: Entry) => {
+                acc = $moment
+                    .duration(acc)
+                    .add($moment.duration(e.duration as string))
+                    .format('HH:mm');
+
+                return acc;
+            }, '00:00');
+
+            return $moment.duration(this.weeklyHours).subtract(total).format('HH:mm');
+        },
     },
     actions: {
         addEntry(entry: Entry) {
