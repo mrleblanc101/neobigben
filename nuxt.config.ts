51b20c194d4a1d03f789a28f1680c1724f0498c8
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-01-05 00:30:23 -0500

    Add formatFallbackMessages option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,7 @@ export default defineNuxtConfig({
             legacy: false,
             fallbackWarn: false,
             missingWarn: false,
+            formatFallbackMessages: true,
         },
     },
 
