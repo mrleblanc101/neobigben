bad69458aca27df16ba815d02be71ae39db59edb
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2023-08-14 14:32:24 -0400

    Fix types

--- src/plugins/moment.ts
+++ src/plugins/moment.ts
@@ -2,26 +2,19 @@ import moment from 'moment';
 import 'moment/dist/locale/fr-ca';
 import momentDurationFormatSetup from 'moment-duration-format';
 import type { LocaleObject } from '#i18n';
-import type { NuxtApp } from '#app';
-import type { ComputedRef } from 'vue';
+import type { Composer } from 'vue-i18n';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const locale = nuxtApp.$i18n.locale.value;
-    const iso = nuxtApp.$i18n.locales.value.find((l: LocaleObject) => l.code === locale).iso;
+    const i18n = nuxtApp.$i18n as Composer;
+    const { locale, locales } = i18n;
+    const iso = (locales.value as LocaleObject[]).find((l) => l.code === locale.value)?.iso;
     momentDurationFormatSetup(moment);
     moment.locale(iso);
 
-    nuxtApp.$i18n.onBeforeLanguageSwitch = (
-        oldLocale: string,
-        newLocale: string,
-        isInitialSetup: boolean,
-        nuxtApp: NuxtApp,
-    ) => {
-        const locales = (nuxtApp.$i18n.locales as ComputedRef).value;
-        const iso = locales.find((l: LocaleObject) => l.code === newLocale).iso;
-
+    nuxtApp.hook('i18n:localeSwitched', ({oldLocale, newLocale}) => {
+        const iso = (locales.value as LocaleObject[]).find((l) => l.code === newLocale)?.iso;
         moment.locale(iso);
-    };
+    });
 
     return {
         provide: {
